43f43fd6afd321566f966d5338711ba448a7020a
Carson Seese <cds17257@gmail.com>
2019-01-06 15:52:34 -0500

    Made things work

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Config struct {
+	DefaultURL string     `json:"default"`
+	Port       string     `json:"port"`
+	Redirects  []Redirect `json:"redirects"`
+}
+
+type Redirect struct {
+	Code string `json:"code"`
+	URL  string `json:"url"`
+}
+
+var config Config
+
+func main() {
+	file, fileErr := ioutil.ReadFile("config.json")
+	if fileErr != nil {
+		fmt.Println(fileErr)
+	}
+
+	fmt.Println("File Opened")
+
+	err := json.Unmarshal(file, &config)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(config)
+
+	fmt.Println("Starting HTTP Server")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+config.Port, nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Request recieved for: " + r.URL.String())
+
+	//Check for default requests
+	if r.URL.String() == "/" || r.URL.String() == "/favicon.ico" {
+		fmt.Println("Sending user to default")
+		http.Redirect(w, r, config.DefaultURL, http.StatusSeeOther)
+	} else {
+
+		//Iterate through redirects
+		for i := range config.Redirects {
+
+			//If redirect is found, send the user
+			if config.Redirects[i].Code == r.URL.String() {
+				fmt.Println("Sending user to: " + config.Redirects[i].URL)
+				http.Redirect(w, r, config.Redirects[i].URL, http.StatusSeeOther)
+			}
+		}
+
+		//If for loop exits without finding anything, send user to default    <--- Is there a better way to do this?
+		fmt.Println("Sending user to default")
+		http.Redirect(w, r, config.DefaultURL, http.StatusSeeOther)
+	}
+}
